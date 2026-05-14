f710ba6ce5e301286de5a0996fc407494ef337b5
Dimitris Marakomihelakis <dmarakom@gmail.com>
2026-05-14 17:01:27 +0300

    Add GitHub Actions workflow for weekly builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://dmarakom6.github.io',
+  base: '/axonas_y_z_website',
+});
